13184f1752bb15f0ed83a90637c8bf22ff60829f
Nate Leonard <nateduane@gmail.com>
2026-01-29 17:56:28 -0500

    added motor controls for a motor(?) I do not know which, help me pls lines 15 to 25 is code on a motor(?)

--- src/main/java/frc/robot/subsystems/intake.java
+++ src/main/java/frc/robot/subsystems/intake.java
@@ -6,12 +6,23 @@ package frc.robot.subsystems;
 
 import com.ctre.phoenix6.hardware.TalonFX;
 
+
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class intake extends SubsystemBase {
   /** Creates a new intake. */
   private TalonFX intakeMotor = new TalonFX(1);
+  public class IntakeSubsystem extends SubsystemBase {
+  private final TalonFX TalonFX = new TalonFX(2);
 
+  public void runIntake() {
+    TalonFX.set(0.8);
+  }
+
+  public void stopIntake() {
+    TalonFX.set(0.0);
+  }
+}
   public intake() {
   }
 
