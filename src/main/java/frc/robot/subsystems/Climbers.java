d4003744413158137abdd26b722a238e47d49312
Caleb <soniclightspeed491@gmail.com>
2026-02-12 18:06:27 -0500

    I WANNA CLIMB!!!!!!!

---
+++ src/main/java/frc/robot/subsystems/Climbers.java
@@ -0,0 +1,34 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import com.ctre.phoenix6.configs.MotorOutputConfigs;
+import com.ctre.phoenix6.configs.TalonFXConfiguration;
+import com.ctre.phoenix6.hardware.DeviceIdentifier;
+import com.ctre.phoenix6.hardware.TalonFX;
+import com.ctre.phoenix6.signals.FeedbackSensorSourceValue;
+import com.ctre.phoenix6.signals.InvertedValue;
+import com.ctre.phoenix6.signals.NeutralModeValue;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class Climbers extends SubsystemBase {
+  /** Creates a new Climb. */
+   private TalonFX climbMotor = new TalonFX(6)
+
+  public void runIntake() {
+    climbMotor.set(0.);
+  }
+
+  public void stopIntake() {
+    climbMotor.set(0.0);
+  }
+
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+}
